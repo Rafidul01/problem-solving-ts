75234f32073c10ad1188240c372902e7593246c7
Rafidul01 <rafidulhasan555@gmail.com>
2026-05-07 02:12:30 +0600

    problem 6 solved

--- solutions.ts
+++ solutions.ts
@@ -75,3 +75,32 @@ const toggleReadStatus = (book : Book) : Book & {isRead : boolean} => {
 
 const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
 console.log(toggleReadStatus(myBook));
+
+
+//Problem 6 
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name:string, age:number){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Student extends Person {
+    grade : string;
+
+    constructor(name: string, age: number, grade: string){
+        super(name,age);
+        this.grade = grade;
+    }
+
+    getDetails(){
+        return (`Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`)
+    }
+}
+
+const student = new Student("Alice", 20, "A");
+console.log(student.getDetails());
